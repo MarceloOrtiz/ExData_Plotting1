1ab9c113982760bebcdcf50ef72d57be92fc4242
MarceloOrtiz <marcelo90ortiz@hotmail.com>
2014-06-07 19:52:02 -0500

    Add 2 files
    plot1.R, plot2.R

---
+++ plot1.R
@@ -0,0 +1,8 @@
+data <- read.table(file = "household_power_consumption.txt",
+	sep = ";", nrows = 2880, skip = 66637)
+
+data$V10 <- strptime(paste(data$V1,data$V2),"%d/%m/%Y %H:%M:%S")
+
+hist(data$V3, xlab = "Global Active Power(kilowatts)",
+	 main = "Global Active Power", col = "red")
+
---
+++ plot2.R
@@ -0,0 +1,8 @@
+data <- read.table(file = "household_power_consumption.txt",
+	sep = ";", nrows = 2880, skip = 66637)
+
+data$V10 <- strptime(paste(data$V1,data$V2),"%d/%m/%Y %H:%M:%S")
+
+plot(data$V10,data$V3, type = "l", xlab ="", ylab = "Global Active Power(kilowatts)")
+
+
