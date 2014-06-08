122fa888da16104bc25a178c7f387043eaf792bb
MarceloOrtiz <marcelo90ortiz@hotmail.com>
2014-06-07 20:15:08 -0500

    Add png files
    I added png files for plot1 and plot2

--- plot1.R
+++ plot1.R
@@ -6,3 +6,5 @@ data$V10 <- strptime(paste(data$V1,data$V2),"%d/%m/%Y %H:%M:%S")
 hist(data$V3, xlab = "Global Active Power(kilowatts)",
 	 main = "Global Active Power", col = "red")
 
+dev.copy(png, file = "plot1.png")
+dev.off()
--- plot2.R
+++ plot2.R
@@ -5,4 +5,5 @@ data$V10 <- strptime(paste(data$V1,data$V2),"%d/%m/%Y %H:%M:%S")
 
 plot(data$V10,data$V3, type = "l", xlab ="", ylab = "Global Active Power(kilowatts)")
 
-
+dev.copy(png, file = "plot2.png")
+dev.off()
